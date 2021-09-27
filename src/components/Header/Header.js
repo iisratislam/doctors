57eedf43e276000ebfe8ca714fb14ccf24807da4
iiisratislam <iiisratislam@gmail.com>
2021-09-27 11:52:05 +0600

    second

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+	return (
+		<div className="header">
+			<h1>Top Doctos of Bangladesh</h1>
+			<p>
+			Doctors typically work for hospitals, urgent care facilities and private clinics to administer treatment and give patients health advice. ... Their job is to prescribe medications , refer patients to specialized medical professionals and conduct tests to diagnose injuries, illnesses or diseases.
+			</p>
+			<h1>Total Fees : 100 million </h1>
+		</div>
+	);
+};
+
+export default Header;
